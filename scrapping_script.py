700ce65a8b2e816eef365657aeb1ee1fae7e3409
Karolpz <carolinelpz34@gmail.com>
2025-12-31 16:22:12 +0100

    add bf4 and test

---
+++ scrapping_script.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://books.toscrape.com/"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
+print(soup.prettify())
